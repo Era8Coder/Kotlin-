22998062e7d4ac5d82e51379f569f5f202ed4d9e
Code_Masher <137721678+Era8Coder@users.noreply.github.com>
2026-01-08 18:29:32 +0530

    Print Function

---
+++ 1.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var x = 7
+    var y = "Anirudh"
+    
+    println("Hello")		// ln for new line <- 
+    println("World $x")		// $ <-> string templates 
+  	println("Crazy ${y.uppercase()}")    // converting to upper case 
+}
