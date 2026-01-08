9e00416404f92b794538a0068e09cc9c3389435f
Code_Masher <137721678+Era8Coder@users.noreply.github.com>
2026-01-08 18:41:22 +0530

    Implement null safety for myName variable
    Add null safety check for nullable variable myName

---
+++ 3.kt
@@ -0,0 +1,8 @@
+fun main(){
+	var myName: String? = null 				// ? implies it is of type null <- 	if no question mark then it can't have nnull data - type 
+    
+    if(myName != null){
+		print(myName)
+    }
+}
+
